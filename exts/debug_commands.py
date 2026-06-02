a2335dd549f649837ba15e654956c83e58a31aa8
안강현 <gimon0331@gmail.com>
2026-06-02 20:40:46 +0900

    Add raw database diagnostics

--- exts/debug_commands.py
+++ exts/debug_commands.py
@@ -1,3 +1,6 @@
+import traceback
+
+import aiomysql
 import discord
 from discord.ext import commands
 
@@ -5,6 +8,7 @@ from discord.ext import commands
 class DebugCommands(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.pool = self.client.pool
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -12,6 +16,19 @@ class DebugCommands(commands.Cog):
             return
         if message.content in {"!rawping", "알티야 rawping", "알티야rawping"}:
             await message.channel.send("raw message listener is working")
+        if message.content in {"!dbcheck", "알티야 dbcheck", "알티야dbcheck"}:
+            try:
+                async with self.pool.acquire() as conn:
+                    async with conn.cursor(aiomysql.DictCursor) as cur:
+                        await cur.execute("SELECT * FROM userdata WHERE id = %s", message.author.id)
+                        user_rows = await cur.fetchall()
+                        await cur.execute("SELECT COUNT(*) AS count FROM userdata")
+                        count_row = await cur.fetchone()
+                await message.channel.send(
+                    f"db is working | userdata_count={count_row['count']} | your_rows={len(user_rows)}"
+                )
+            except Exception:
+                await message.channel.send("dbcheck failed:\n```py\n" + traceback.format_exc()[-1500:] + "\n```")
 
     @commands.command(name="봇진단", aliases=["debugping"])
     async def debug_ping(self, ctx):
