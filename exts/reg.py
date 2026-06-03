fde9a275b0ca18af21e0cf668cc8de64e4f9594d
안강현 <gimon0331@gmail.com>
2026-06-03 11:17:11 +0900

    Use button confirmation for registration

--- exts/reg.py
+++ exts/reg.py
@@ -1,6 +1,7 @@
 import discord, json, asyncio, aiomysql
 from discord.ext import commands
 from utils import errors, checks
+from utils.views import ask_confirm
 
 
 def get_embed(title, description='', color=0xCCFFFF):
@@ -19,87 +20,47 @@ class reg(commands.Cog):
 
     @commands.command(name='탈퇴')
     async def _logout(self, ctx):
-        msg = await ctx.send(embed=get_embed("📝 | **알티봇 서비스에서 탈퇴하시겠습니까?**", "탈퇴시, 돈과 강화 목록을 포함한 모든 데이터가 영구적으로 삭제되며 복구할수 없습니다."))
-        emjs = ['<a:yes:698461934198063104>', '<a:no:698461934613168199>']
-
-        for em in emjs:
-            await msg.add_reaction(em)
-
-        def check(reaction, user):
-            return user == ctx.author and msg.id == reaction.message.id and str(reaction.emoji) in emjs
-
-        try:
-            reaction, user = await self.client.wait_for('reaction_add', check=check, timeout=20)
-        except asyncio.TimeoutError:
-            await asyncio.gather(
-                msg.delete(),
-                ctx.send(embed=get_embed('⏰ | 시간이 초과되었습니다!', "", 0xFF0000)),
-            )
+        confirmed, msg = await ask_confirm(
+            ctx,
+            embed=get_embed("회원 탈퇴 확인", "탈퇴하면 돈과 강화 목록을 포함한 모든 데이터가 삭제됩니다."),
+            timeout=30,
+        )
+        if confirmed is None:
+            await ctx.send(embed=get_embed('시간이 초과되었습니다.', "", 0xFF0000))
+            return
+        if confirmed is False:
+            await ctx.send(embed=get_embed('취소되었습니다.', "", 0xFF0000))
             return
 
-        e = str(reaction.emoji)
+        async with self.pool.acquire() as conn:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute('DELETE FROM reinforce WHERE id = %s', ctx.author.id)
+                await cur.execute('DELETE FROM userdata WHERE id = %s', ctx.author.id)
 
-        if e == '<a:yes:698461934198063104>':
-            async with self.pool.acquire() as conn:
-                async with conn.cursor(aiomysql.DictCursor) as cur:
-                    await cur.execute('DELETE FROM reinforce WHERE id = %s', ctx.author.id)
-                    await cur.execute('DELETE FROM userdata WHERE id = %s', ctx.author.id)
-
-            await asyncio.gather(
-                msg.delete(),
-                ctx.send(embed=get_embed('<a:yes:698461934198063104> | 탈퇴에 성공했습니다!', "", 0xCCFFFF)),
-            )
-            return
-
-        if e == '<a:no:698461934613168199>':
-            await asyncio.gather(
-                msg.delete(),
-                ctx.send(embed=get_embed('<a:no:698461934613168199> | 취소 되었습니다!', "", 0xFF0000)),
-            )
-            return
+        await ctx.send(embed=get_embed('탈퇴에 성공했습니다.', "", 0xCCFFFF))
 
     @commands.command(name='가입', aliases=['나도'])
     async def _login(self, ctx):
-        msg = await ctx.send(embed=get_embed("📝 | **알티봇 서비스에 가입하시겠습니까?**", f"**NAME** = {ctx.author}\n**ID** = {ctx.author.id}"))
-        emjs = ['<a:yes:698461934198063104>', '<a:no:698461934613168199>']
-
-        for em in emjs:
-            await msg.add_reaction(em)
-
-        def check(reaction, user):
-            return user == ctx.author and msg.id == reaction.message.id and str(reaction.emoji) in emjs
-
-        try:
-            reaction, user = await self.client.wait_for('reaction_add', check=check, timeout=20)
-        except asyncio.TimeoutError:
-            await asyncio.gather(
-                msg.delete(),
-                ctx.send(embed=get_embed('⏰ | 시간이 초과되었습니다!', "", 0xFF0000)),
-            )
+        confirmed, msg = await ask_confirm(
+            ctx,
+            embed=get_embed("회원 가입 확인", f"NAME = {ctx.author}\nID = {ctx.author.id}"),
+            timeout=30,
+        )
+        if confirmed is None:
+            await ctx.send(embed=get_embed('시간이 초과되었습니다.', "", 0xFF0000))
+            return
+        if confirmed is False:
+            await ctx.send(embed=get_embed('취소되었습니다.', "", 0xFF0000))
             return
 
-        e = str(reaction.emoji)
+        async with self.pool.acquire() as conn:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(
+                    'INSERT INTO userdata (id, money, bank, adminuser, blacklist) VALUES (%s, %s, %s, %s, %s)',
+                    (ctx.author.id, '5000', '0', 0, 0),
+                )
 
-        if e == '<a:yes:698461934198063104>':
-            async with self.pool.acquire() as conn:
-                async with conn.cursor(aiomysql.DictCursor) as cur:
-                    await cur.execute(
-                        'INSERT INTO userdata (id, money, bank, adminuser, blacklist) VALUES (%s, %s, %s, %s, %s)',
-                        (ctx.author.id, '5000', '0', 0, 0),
-                    )
-
-            await asyncio.gather(
-                msg.delete(),
-                ctx.send(embed=get_embed('<a:yes:698461934198063104> | 가입에 성공했습니다!', "", 0xCCFFFF)),
-            )
-            return
-
-        if e == '<a:no:698461934613168199>':
-            await asyncio.gather(
-                msg.delete(),
-                ctx.send(embed=get_embed('<a:no:698461934613168199> | 취소 되었습니다!', "", 0xFF0000)),
-            )
-            return
+        await ctx.send(embed=get_embed('가입에 성공했습니다.', "", 0xCCFFFF))
 
 
 async def setup(client):
